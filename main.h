95a14038e88cdd3db3968411d8131d10d8ca0321
Xtopher <ibebuchi@hotmail.com>
2023-04-26 02:25:48 +0000

    Print

--- main.h
+++ main.h
@@ -1,10 +1,4 @@
-********* FUNCTIONS ******************/
-
-/* Funtions to print chars and strings */
-int print_char(va_list types, char buffer[],
-			int flags, int width, int precision, int size);
-int print_string(va_list types, char buffer[],
-			int flags, int width, int preci#ifndef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
 #include <stdio.h>
@@ -49,7 +43,13 @@ int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
 
-/*********sion, int size);
+/****************** FUNCTIONS ******************/
+
+/* Funtions to print chars and strings */
+int print_char(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
 int print_percent(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
